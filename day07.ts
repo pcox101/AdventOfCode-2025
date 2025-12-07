c5c9ff99dae0ebe9ad7c01d49f6563c1e3368802
Peter Cox <peter.cox@kingfisher.com>
2025-12-07 18:10:11 +0000

    Day 7 needed a bit of refactoring...

--- day07.ts
+++ day07.ts
@@ -25,27 +25,11 @@ for (let row = 0; row < gameboard.length - 1; row++)
             let d1 = gameboard[row+1][col];
             if (d1 == '^') {
                 part1++;
-                let l = gameboard[row+1][col-1];
-                let lVal = cVal;
-                if (l != '.') {
-                    lVal = lVal + parseInt(l);
-                }
-                gameboard[row+1][col-1] = lVal.toString();
-                let r = gameboard[row+1][col+1];
-                let rVal = cVal;
-                if (r != '.') {
-                    rVal = rVal + parseInt(r);
-                }
-                gameboard[row+1][col+1] = rVal.toString();
-
+                updateOneBelow(row, col - 1, cVal);
+                updateOneBelow(row, col + 1, cVal)
             }
             else {
-                let s = gameboard[row+1][col];
-                let sVal = cVal;
-                if (s != '.') {
-                    sVal = sVal + parseInt(s);
-                }
-                gameboard[row+1][col] = sVal.toString();
+                updateOneBelow(row, col, cVal);
             }
         }
     }
@@ -63,6 +47,15 @@ for (let c = 0; c < gameboard[gameboard.length - 1].length; c++) {
 
 console.log("Part 1: " + part1);
 console.log("Part 2: " + part2);
+
+function updateOneBelow(currentRow: number, newCol: number, cVal: number) {
+    let l = gameboard[currentRow + 1][newCol];
+    let lVal = cVal;
+    if (l != '.') {
+        lVal = lVal + parseInt(l);
+    }
+    gameboard[currentRow + 1][newCol] = lVal.toString();
+}
 
 function outputGameBoard() {
     for (let r = 0; r < gameboard.length; r++) {
